072810eeab6e398342a67bea0c40a16cee172aa7
Gabriele Fontana <gafreax@gmail.com>
2015-10-07 22:56:04 +0200

    Modifiche della serata

--- php55-generators.php
+++ php55-generators.php
@@ -2,10 +2,15 @@
 // php55-generators.php
 // @author Gabriele Fontana <gafreax@gmail.com>
 // Some Example for php generators
+
+// http://php.net/manual/en/class.generator.php
+
+
 if(count($argv) !== 3 || is_numeric($argv[1])===false || is_numeric($argv[2])===false) {
 	echo 'Usage: ' . $argv[0] . ' <number_start>  <number_end>'. PHP_EOL;
 	die();
 }
+/// --- START CICCIA --- 
 
 // generators
 function oddinrange($start,$limit) {
@@ -14,3 +19,8 @@ function oddinrange($start,$limit) {
 
 foreach (oddinrange($argv[1],$argv[2]) as $number)
 	echo 'Processo '. $number. PHP_EOL;
+
+
+echo PHP_EOL;
+
+var_dump(oddinrange($argv[1],$argv[2]));
--- php55-list.php
+++ php55-list.php
@@ -2,6 +2,10 @@
 // php55-list.php
 // @author Gabriele Fontana <gafreax@gmail.com>
 // Some Example for php list in foreach
-$array = [[3,55],[123,66]];
-foreach($array as list($a,$b))
-	echo 'Listato ' . $a . ' ' . $b . PHP_EOL;
+$array = [1,[2,3],[2,[3,4]]];
+print_r($array);
+
+foreach($array as $puppa) echo $puppa;
+echo PHP_EOL;
+foreach($array as list($a,list($b,$c)))
+	echo 'Listato ' . $a . ' ' . $b . ' ' . $c .  PHP_EOL;
